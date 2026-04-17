59cd37dd40e22ce65adec08e9b5744c6178ec3dc
Akbar Dhanaliwala <akbar.dhanaliwala@gmail.com>
2026-04-16 20:08:27 -0700

    add correct PID for e36313a

--- cli/impl/query_instruments.py
+++ cli/impl/query_instruments.py
@@ -64,7 +64,7 @@ SUPPORTED_USB: Dict[str, Dict[str, str | List[str] | None]] = {
     "EA_PSB_10080_60":   {"vid": "232e", "pid": "0053", "net_type": ["power-supply", "solar"]},
     "EA_PSB_10060_60":   {"vid": "232e", "pid": "0053", "net_type": ["power-supply", "solar"]},
     "KEYSIGHT_E36233A":  {"vid": "2a8d", "pid": "3302", "net_type": ["power-supply"]}, # 2-channel
-    "KEYSIGHT_E36313A":  {"vid": "2a8d", "pid": "????", "net_type": ["power-supply"]},
+    "KEYSIGHT_E36313A":  {"vid": "2a8d", "pid": "1202", "net_type": ["power-supply"]},
     "KEYSIGHT_E36312A":  {"vid": "2a8d", "pid": "1102", "net_type": ["power-supply"]},
 
     # battery
